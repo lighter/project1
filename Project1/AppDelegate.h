e7da0cacfb70375309c6722fcb4c3ba64406af0a
lighter <apple.xva@gmail.com>
2014-08-30 13:19:52 +0800

    Initial Commit

---
+++ Project1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project1
+//
+//  Created by lighter on 2014/8/30.
+//  Copyright (c) 2014年 lighter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
